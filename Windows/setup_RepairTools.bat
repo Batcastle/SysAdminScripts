41297eae37762ea4b8d96635099de5d92a9bae96
Thomas Castleman <31315409+Batcastle@users.noreply.github.com>
2024-06-12 09:47:33 -0400

    Bug fix for setup
     - Updated ClamAV version
     - Updated file names
     - Fixed 7z CLI argument

--- Windows/setup_RepairTools.bat
+++ Windows/setup_RepairTools.bat
@@ -4,7 +4,7 @@
 
 :: Make sure we are not running as root. That makes this much more difficult and we don't need it.
 net session >nul 2>&1
-if %errorLevel% eq 0 (
+if %errorLevel% == 0 (
     echo Running as administrator. Please run normally.
     timeout /t -1
     exit /b 1
@@ -15,13 +15,13 @@ if %errorLevel% eq 0 (
 echo Downloading necessary files...
 
 :: AVs
-curl.exe --output ClamAV.zip https://www.clamav.net/downloads/production/clamav-1.3.0.win.x64.zip
+curl.exe --output ClamAV.zip https://www.clamav.net/downloads/production/clamav-1.3.1.win.x64.zip
 curl.exe --output GetSusp64.exe https://downloadcenter.trellix.com/products/mcafee-avert/getsusp/getsusp64.exe
 curl.exe --output Stinger64.exe https://downloadcenter.trellix.com/products/mcafee-avert/Stinger/stinger64.exe
 
 :: Batch Scripts
-curl.exe --output admin_check_for_corruption.bat https://raw.githubusercontent.com/Batcastle/SysAdminScripts/master/Windows/check_for_corruption.bat
-curl.exe --output admin_clam_scan.bat https://raw.githubusercontent.com/Batcastle/SysAdminScripts/master/Windows/clam_scan.bat
+curl.exe --output admin_check_for_corruption.bat https://raw.githubusercontent.com/Batcastle/SysAdminScripts/master/Windows/admin_check_for_corruption.bat
+curl.exe --output admin_clam_scan.bat https://raw.githubusercontent.com/Batcastle/SysAdminScripts/master/Windows/admin_clam_scan.bat
 curl.exe --output autorun.bat https://raw.githubusercontent.com/Batcastle/SysAdminScripts/master/Windows/autorun.bat
 
 :: Config files
@@ -32,7 +32,7 @@ curl.exe --output 7z.exe https://www.7-zip.org/a/7zr.exe
 
 :: Extract ClamAV.zip
 echo Extracting archives...
-7z.exe e ClamAV.zip -y -o ClamAV
+7z.exe e ClamAV.zip -y -oClamAV
 
 :: Configure ClamAV
 echo Configuring tools...
